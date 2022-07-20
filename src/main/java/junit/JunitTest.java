bdb7a0049af9e3bc2fdadcd8b76df635e7c1e04b
Bradly-s <3308688609@qq.com>
2022-07-20 21:51:19 +0800

    Junit

--- src/main/java/junit/JunitTest.java
+++ src/main/java/junit/JunitTest.java
@@ -2,7 +2,6 @@ package junit;
 
 import org.junit.Test;
 
-//一般的话是创建main方法,执行main方法才能执行
 public class JunitTest {
 //    public static void main(String[] args) {
 //        new JunitTest().add();
@@ -13,7 +12,6 @@ public class JunitTest {
 //        System.out.println(a+b);
 //    }
 
-    //而增加@test之后，可以直接点击运行，也可以进行调试
     @Test
     public void add(){
         int a=5;
